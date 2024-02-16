879b7901b56a68699c236304bc56088f9d64fe65
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-02-16 12:26:15 +0200

    added unit tests (store & movie)

--- __tests__/videoStore.test.js
+++ __tests__/videoStore.test.js
@@ -73,3 +73,75 @@ describe("Test all client get endpoints", () => {
     
     
       });
+
+
+      // **********  Test Movie GET all endpoints ********************* 
+      describe("Test all the stores GET endpoints", () => {
+
+         // Connect to the database
+            beforeEach(async () => {
+                await mongoose.connect(process.env.MONGODB_URL);
+            });
+            
+            // Closing database connection
+            afterEach(async () => {
+                await mongoose.connection.close();
+            });
+            
+              it("Should return all of the store names", async () => {
+                const res = await request(app).get("/stores");
+                expect(res.statusCode).toBe(200);
+                expect(res.body.length).toBeGreaterThan(1);
+                expect(res.body).toEqual(
+                expect.arrayContaining([
+                  expect.objectContaining({
+                  Name: 'West Peak'
+                  })
+                ])
+                )
+              });
+        
+              it("should return a single store", async () => {
+                const res = await request(app).get("/stores/65c35a63f76457be4307c2c5");
+                expect(res.statusCode).toBe(200);
+                expect(res.body.Name).toEqual("Falcon Falls");
+              });
+        
+        
+          });
+
+
+      // **********  Test Movie GET all endpoints ********************* 
+      describe("Test all movie get endpoints", () => {
+
+        // Connect to the database
+           beforeEach(async () => {
+               await mongoose.connect(process.env.MONGODB_URL);
+           });
+           
+           // Closing database connection
+           afterEach(async () => {
+               await mongoose.connection.close();
+           });
+           
+             it("Should return all of the movies", async () => {
+               const res = await request(app).get("/movies");
+               expect(res.statusCode).toBe(200);
+               expect(res.body.length).toBeGreaterThan(3);
+               expect(res.body).toEqual(
+               expect.arrayContaining([
+                 expect.objectContaining({
+                 Name: 'Finding Nemo'
+                 })
+               ])
+               )
+             });
+       
+             it("should return a single movie", async () => {
+               const res = await request(app).get("/movies/65c34a97f76457be4307c2af");
+               expect(res.statusCode).toBe(200);
+               expect(res.body.Name).toEqual("The Gods Must Be Crazy");
+             });
+       
+       
+         });
